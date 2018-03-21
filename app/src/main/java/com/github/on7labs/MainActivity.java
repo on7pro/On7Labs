edd87f6acfb9137f217306763b73a29a159c5e9e
androidlover5842 <androidlover5842@gmail.com>
2018-03-21 13:18:52 +0530

    app: show user profile

--- app/src/main/java/com/github/on7labs/MainActivity.java
+++ app/src/main/java/com/github/on7labs/MainActivity.java
@@ -1,5 +1,9 @@
 package com.github.on7labs;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,10 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private View NavView;
+    private TextView textViewName,textViewEmail;
+    private FirebaseAuth firebaseAuth;
+    private String email,name;
+    private Uri profileUrl;
+    private ImageView imageViewProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +45,19 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        firebaseAuth=FirebaseAuth.getInstance();
+        name=firebaseAuth.getCurrentUser().getDisplayName();
+        email=firebaseAuth.getCurrentUser().getEmail();
+        profileUrl=firebaseAuth.getCurrentUser().getPhotoUrl();
         NavigationView navigationView =  findViewById(R.id.nav_view);
+        NavView= navigationView.inflateHeaderView(R.layout.nav_header_main);
+        textViewName=NavView.findViewById(R.id.tv_nav_name);
+        textViewEmail=NavView.findViewById(R.id.tv_nav_email);
+        imageViewProfile=NavView.findViewById(R.id.nav_imageView);
+
+        textViewName.setText(name);
+        textViewEmail.setText(email);
+        new ProfileTask().execute();
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -68,5 +99,26 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private class ProfileTask extends AsyncTask<Void,Void,Void>{
+        private Bitmap bmp;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL  url = new URL(profileUrl.toString());
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            imageViewProfile.setImageBitmap(bmp);
+        }
     }
 }
